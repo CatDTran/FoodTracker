4da206fe85a75036343ca26068a25b6504916d26
CarrotOnMacPro <trandinhcat@gmail.com>
2016-07-01 02:04:44 -0500

    Fix the damn disconnection between Meal Name Label and mealNameLabel

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -9,13 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    //MARK: Properties
+    // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNamelLabel: UILabel!
+    @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
+    
 }
 
