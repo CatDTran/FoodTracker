a54c9731843f46bcc8b233024e9b3d9d34316aee
CarrotOnMacPro <trandinhcat@gmail.com>
2016-07-03 12:36:14 -0500

    Set meal name with text entered in TextField

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,14 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // decleare this class as delegate for TextField
+        nameTextField.delegate = self//"self" refer to this ViewController class
+    }
+    // MARK: UITextFieldDelegate
+    func textFieldShouldReturn(textField : UITextField) -> Bool{
+        //Resign from first responder status; hide keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    //This method is called after textFieldShouldReturn() method
+    func textFieldDidEndEditing(textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
     // MARK: Actions
     @IBAction func setDefaultLabelText(sender: UIButton) {
