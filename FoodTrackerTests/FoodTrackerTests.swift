af9a4e6c52ff59a8ce843cbb18d967f0b6ceb560
CarrotOnMacPro <trandinhcat@gmail.com>
2016-07-10 18:12:11 -0500

    Added Table View Controller to the scene and make it the entry point

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -10,27 +10,14 @@ import XCTest
 @testable import FoodTracker
 
 class FoodTrackerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+    //MARK: FoodTracker Tests
+    func testMealInitialization(){
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)//sucess test case
+        XCTAssertNotNil(potentialItem)//test for not nil
+        let noName = Meal(name: "", photo: nil, rating: 0)//failure case with no name
+        XCTAssertNil(noName, "Empty name is invalid")//test for nil
+        let badRating = Meal(name: "Really bad rating", photo: nil, rating: -1)//failure case with negative rating
+        XCTAssertNil(badRating, "Negative rating sucks, be positive")//test for not nil
     }
     
 }
