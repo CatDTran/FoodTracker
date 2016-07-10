5ee9a52930caef2055200011574512e693ce0e44
CarrotOnMacPro <trandinhcat@gmail.com>
2016-07-10 11:43:46 -0500

    Added some properties and initializer to Meal.swift

--- Meal.swift
+++ Meal.swift
@@ -10,7 +10,18 @@ import UIKit
 class Meal{
     //MARK: Properties
     var name : String
-    var photo: UIImage
+    var photo: UIImage?
     var rating: Int
+    
+    //MARK: Initialization
+    init?(name: String, photo: UIImage?, rating: Int){
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        if name.isEmpty || rating < 0{//initilazation will return nil if no name is enter or rating is negative
+            return nil
+        }
+        
+    }
     
 }
