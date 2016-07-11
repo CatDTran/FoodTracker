5e1cceb583a5e4ed888b8676a77670909cb81a91
CarrotOnMacPro <trandinhcat@gmail.com>
2016-07-10 20:04:29 -0500

    Added new class UITableViewCell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Sriracha Sauce on 7/10/16.
+//  Copyright © 2016 Sriracha Sauce. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
